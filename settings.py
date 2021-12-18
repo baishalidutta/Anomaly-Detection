df6046fb3b882fe1d9340e2cdc4a09e26d7a6470
Baishali Dutta <me@itsbaishali.com>
2021-12-18 07:49:04 +0100

    Added Settings

---
+++ settings.py
@@ -0,0 +1,19 @@
+import os
+from os.path import join, dirname
+
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DELAY = 0.01
+NUM_PARTITIONS = 3
+OUTLIERS_GENERATION_PROBABILITY = 0.2
+KAFKA_BROKER = "localhost:9092"
+TRANSACTIONS_TOPIC = "transactions"
+TRANSACTIONS_CONSUMER_GROUP = "transactions"
+ANOMALIES_TOPIC = "anomalies"
+ANOMALIES_CONSUMER_GROUP = "anomalies"
+
+SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+SLACK_CHANNEL = "data-consumption"
