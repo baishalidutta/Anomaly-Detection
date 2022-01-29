4abc49a9d66b6e67930eea6d4e4731ad11b9d2bf
Baishali Dutta <me@itsbaishali.com>
2022-01-29 13:46:02 +0100

    Reformatted Source

--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
 import os
-from os.path import join, dirname
-
 from dotenv import load_dotenv
+from os.path import join, dirname
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
